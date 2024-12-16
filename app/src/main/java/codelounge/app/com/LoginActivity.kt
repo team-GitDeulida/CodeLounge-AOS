23eb4f0a1d4c343319341628ba7f743558ce60e3
mac <esse3404028@naver.com>
2024-12-16 15:08:50 +0900

    DESIGN: 구글 로그인 ui 변경

--- app/src/main/java/codelounge/app/com/LoginActivity.kt
+++ app/src/main/java/codelounge/app/com/LoginActivity.kt
@@ -1,19 +1,78 @@
 package codelounge.app.com
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import codelounge.app.com.ui.theme.BackgroundColor
 
 @Composable
 fun loginActivity(navController: NavController) {
-    Column {
-    Text("Code Lounge")
-        Button(onClick = { navController.navigate("home") }) {
-            Text("Login")
+    Column(
+        modifier = Modifier.fillMaxSize().background(BackgroundColor),
+        verticalArrangement = Arrangement.SpaceBetween, // 요소 간의 간격을 화면 전체로 분배
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.weight(4f)) // 상단 여백 채우기
+
+        Text("Code Lounge", fontSize = 50.sp, color = Color.White) // 화면 중간에 유지
+
+        Spacer(modifier = Modifier.weight(2.5f)) // Text 아래 공간 채우기
+
+        googleLoginButton() // 하단에 배치
+        Spacer(modifier = Modifier.weight(2.5f))
+    }
+}
+
+
+@Composable
+fun googleLoginButton() {
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 60.dp)
+            .fillMaxWidth()
+            .height(40.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.google_logo),
+                contentDescription = "Google Logo",
+                modifier = Modifier
+                    .height(18.dp)
+                    .padding(start = 30.dp)
+            )
+            Text(
+                text = "google 계정으로 로그인",
+                modifier = Modifier
+                    .padding(start = 30.dp)
+            )
         }
     }
 }
@@ -22,4 +81,10 @@ fun loginActivity(navController: NavController) {
 @Composable
 fun LoginPreview() {
     loginActivity(navController = rememberNavController())
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GoogleLoginButtonPreview() {
+    googleLoginButton()
 }
