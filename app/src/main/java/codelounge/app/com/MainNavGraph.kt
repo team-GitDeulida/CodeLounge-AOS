10580017f034ef5f04352826b2d930f04e79c523
mac <esse3404028@naver.com>
2024-12-17 17:11:51 +0900

    FIX: 데이터베이스 저장을 위해 입력문 해결

--- app/src/main/java/codelounge/app/com/MainNavGraph.kt
+++ app/src/main/java/codelounge/app/com/MainNavGraph.kt
@@ -6,10 +6,10 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 
 @Composable
-fun MainNavGraph(navController: NavHostController) {
+fun MainNavGraph(navController: NavHostController, firebaseData: Map<String, Any?>) {
     NavHost(navController = navController, startDestination = "login") {
         composable("login") { loginActivity(navController) }
-        composable("home") { Combine(navController) }
+        composable("home") { Combine(navController,firebaseData) }
         composable("listContents") { ListContents(navController) }
     }
 }
