f57e7300a573f18e489fe4ecde79afd6667454ae
mac <esse3404028@naver.com>
2024-12-17 09:25:53 +0900

    FEAT: 깃허브 로그인 추가

--- app/src/main/java/codelounge/app/com/LoginActivity.kt
+++ app/src/main/java/codelounge/app/com/LoginActivity.kt
@@ -2,6 +2,7 @@ package codelounge.app.com
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,18 +11,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,25 +33,28 @@ fun loginActivity(navController: NavController) {
         verticalArrangement = Arrangement.SpaceBetween, // 요소 간의 간격을 화면 전체로 분배
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Spacer(modifier = Modifier.weight(4f)) // 상단 여백 채우기
+        Spacer(modifier = Modifier.weight(2.5f)) // 상단 여백 채우기
 
         Text("Code Lounge", fontSize = 50.sp, color = Color.White) // 화면 중간에 유지
 
-        Spacer(modifier = Modifier.weight(2.5f)) // Text 아래 공간 채우기
+        Spacer(modifier = Modifier.weight(3f)) // Text 아래 공간 채우기
 
-        googleLoginButton() // 하단에 배치
-        Spacer(modifier = Modifier.weight(2.5f))
+        githubLoginButton(navController = navController) // 하단에 배치
+        Spacer(modifier = Modifier.height(10.dp))
+        googleLoginButton(navController = navController) // 하단에 배치
+        Spacer(modifier = Modifier.weight(1f))
     }
 }
 
 
 @Composable
-fun googleLoginButton() {
+fun googleLoginButton(navController: NavController) {
     Card(
         modifier = Modifier
             .padding(horizontal = 60.dp)
             .fillMaxWidth()
-            .height(40.dp),
+            .height(40.dp)
+            .clickable { navController.navigate("mainActivity") },
         colors = CardDefaults.cardColors(containerColor = Color.White)
     ) {
         Row(
@@ -77,6 +77,37 @@ fun googleLoginButton() {
     }
 }
 
+@Composable
+fun githubLoginButton(navController: NavController) {
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 60.dp)
+            .fillMaxWidth()
+            .height(40.dp)
+            .clickable { navController.navigate("mainActivity") },
+        colors = CardDefaults.cardColors(containerColor = Color.Black)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.github_mark),
+                contentDescription = "Github Logo",
+                modifier = Modifier
+                    .height(18.dp)
+                    .padding(start = 30.dp)
+            )
+            Text(
+                text = "github 계정으로 로그인",
+                color = Color.White,
+                modifier = Modifier
+                    .padding(start = 30.dp)
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun LoginPreview() {
@@ -86,5 +117,11 @@ fun LoginPreview() {
 @Preview(showBackground = true)
 @Composable
 fun GoogleLoginButtonPreview() {
-    googleLoginButton()
+    googleLoginButton(navController = rememberNavController())
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GithubLoginButtonPreview() {
+    githubLoginButton(navController = rememberNavController())
 }
