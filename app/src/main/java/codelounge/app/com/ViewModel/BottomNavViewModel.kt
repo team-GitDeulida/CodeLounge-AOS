8b6b92b195fce9b6402e9b930c18e7f953272758
esse3 <esse3404028@naver.com>
2025-01-11 02:45:10 +0900

    Style: web제외 profile

--- app/src/main/java/codelounge/app/com/ViewModel/BottomNavViewModel.kt
+++ app/src/main/java/codelounge/app/com/ViewModel/BottomNavViewModel.kt
@@ -1,6 +1,7 @@
 package codelounge.app.com.ViewModel
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Android
 import androidx.compose.material.icons.filled.PhoneIphone
 import androidx.compose.material.icons.filled.School
@@ -13,6 +14,6 @@ class BottomNavViewModel : ViewModel() {
         BottomNavItem(label = "CS", icon = Icons.Filled.School, route = "home"), // Computer Science 관련 아이콘
         BottomNavItem(label = "Android", icon = Icons.Filled.Android, route = "android"), // Android 아이콘
         BottomNavItem(label = "iOS", icon = Icons.Filled.PhoneIphone, route = "ios"), // iOS 아이콘
-        BottomNavItem(label = "Web", icon = Icons.Filled.Web, route = "Web") // Frontend 관련 아이콘
+        BottomNavItem(label = "Profile", icon = Icons.Filled.AccountCircle, route = "profile") // Profile 관련 아이콘
     )
 }
