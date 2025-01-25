34fb310178e8ad07a07d9e10962bf9ee52d71673
esse3 <esse3404028@naver.com>
2025-01-25 17:04:56 +0900

    Feat: 로그인 유지기능 추가

--- app/src/main/java/codelounge/app/com/MainActivity.kt
+++ app/src/main/java/codelounge/app/com/MainActivity.kt
@@ -3,14 +3,17 @@ package codelounge.app.com
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
 import codelounge.app.com.Model.FirebaseRepository
 import codelounge.app.com.Model.LoginRepository
 import codelounge.app.com.View.NavigationGraph
 import codelounge.app.com.ViewModel.FirebaseViewModel
-import codelounge.app.com.theme.CodeLoungeTheme
 import codelounge.app.com.ViewModel.FirebaseViewModelFactory
+import codelounge.app.com.theme.CodeLoungeTheme
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : ComponentActivity() {
@@ -26,6 +29,16 @@ class MainActivity : ComponentActivity() {
                     factory = FirebaseViewModelFactory(repository)
                 )
                 val loginRepository = LoginRepository()
+
+                LaunchedEffect(Unit) {
+                    val currentUser = FirebaseAuth.getInstance().currentUser
+                    if (currentUser != null) {
+                        // 사용자가 로그인되어 있으면 home 화면으로 이동
+                        navController.navigate("home") {
+                            popUpTo("login") { inclusive = true }
+                        }
+                    }
+                }
 
                 NavigationGraph(
                     navController = navController,
