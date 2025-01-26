af5a0314d446a62048df50888c874655d9e98596
esse3 <esse3404028@naver.com>
2025-01-27 01:47:25 +0900

    Design: 배너 이미지 수정

--- app/src/main/java/codelounge/app/com/View/LoginScreen.kt
+++ app/src/main/java/codelounge/app/com/View/LoginScreen.kt
@@ -1,19 +1,23 @@
 package codelounge.app.com.View
 
+import androidx.compose.foundation.Image
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import codelounge.app.com.Model.LoginRepository
+import codelounge.app.com.R
 import codelounge.app.com.ViewModel.LoginViewModel
 import codelounge.app.com.ViewModel.LoginViewModelFactory
 import codelounge.app.com.theme.BackgroundColor
@@ -34,18 +38,18 @@ fun LoginScreen(navController: NavController, repository: LoginRepository) {
     ) {
         Spacer(modifier = Modifier.weight(2.5f))
         Title()
-        Spacer(modifier = Modifier.weight(3f))
+        Spacer(modifier = Modifier.weight(2f))
         GoogleLoginButton(navController = navController, loginViewModel = loginViewModel)
-        Spacer(modifier = Modifier.weight(1f))
+        Spacer(modifier = Modifier.weight(2f))
     }
 }
 
 @Composable
 fun Title() {
-    Text(
-        text = "Code Lounge",
-        fontSize = 50.sp,
-        color = Color.White
+    Image(
+        painter = painterResource(id = R.drawable.code_lounge_banner),
+        contentDescription = null,
+        modifier = Modifier.fillMaxWidth()
     )
 }
 
